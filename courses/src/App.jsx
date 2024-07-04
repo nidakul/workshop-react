8f5ec7faa9cf0e9bd8ce47ee182a02a9cbbe8c10
nidakul <nidakul@hotmail.com>
2024-07-05 01:09:07 +0300

    end of courses example

--- courses/src/App.jsx
+++ courses/src/App.jsx
@@ -5,18 +5,20 @@ import './App.css'
 import Header from './Header'
 import { courses } from './Data'
 import Course from './Course'
+import "./css/Course.css"
 
 function App() {
 
   return (
     <>
       <Header />
-      {
-        courses?.map((course) => (
-          <Course key={course.id} course={course}></Course>
-
-        ))
-      }
+      <div className='course-main'>
+        {
+          courses?.map((course) => (
+            <Course key={course.id} course={course}></Course>
+          ))
+        }
+      </div>
     </>
   )
 }
--- courses/src/Course.jsx
+++ courses/src/Course.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import "./css/Course.css"
 
 const Course = ({ course }) => {
     const { id, title, description, price, link, image } = course;
     return (
-        <div className='course'>
-            <div>
+        <div className='course-main'>
+            <div className='course'>
                 <img src={image} width={250} height={150} />
-                <h4>{title}</h4>
-                <h5>{description}</h5>
-                <h3>{price}</h3>
-                <a href={link}>satın almak için</a>
+                <h4 className='course-title'>{title}</h4>
+                <p className='course-desc'>{description}</p>
+                <h3 className='course-price'>{price} ₺</h3>
+                <div className='course-link' ><a href={link}>satın almak için</a> </div>
             </div>
         </div>
     )
