9f3c3466a335f49ce02eac26461afc6cd4a950dd
nidakul <nidakul@hotmail.com>
2024-07-06 02:16:05 +0300

    created todo-app

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import TodoCreate from './components/TodoCreate'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <TodoCreate />
+    </div>
+  )
+}
+
+export default App
