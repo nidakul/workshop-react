7e9735329dbb3f582d5a120be389beb06fe261e6
nidakul <nidakul@hotmail.com>
2024-07-08 00:43:52 +0300

    redux 1

---
+++ redux/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './redux/counterSlice'
+
+function App() {
+
+  const { value } = useSelector((store) => store.counter);
+  console.log(value);
+
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <div>{value}</div>
+      <div>
+        <button onClick={() => dispatch(increment())}>Arttır</button>
+        <button onClick={() => dispatch(decrement())}>Azalt</button>
+      </div>
+    </>
+  )
+}
+
+export default App
