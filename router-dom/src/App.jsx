aa0121d175e2a97213f5071ca42f05d63cf55698
nidakul <nidakul@hotmail.com>
2024-07-10 18:20:03 +0300

    useNavigate and useParams example

--- router-dom/src/App.jsx
+++ router-dom/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from "react-router-dom";
-import Home from '../pages/Home';
-import About from '../pages/About';
-import Products from '../pages/Products';
-import Contact from '../pages/Contact';
-import NotFound from '../pages/NotFound';
-import Header from '../components/Header';
-import EmployeeAbout from '../pages/EmployeeAbout';
-import CompanyAbout from '../pages/CompanyAbout';
+import Home from './pages/Home';
+import EmployeeAbout from './pages/EmployeeAbout';
+import CompanyAbout from './pages/CompanyAbout';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+import About from './pages/About';
+import Header from './components/Header';
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
 
@@ -25,6 +26,7 @@ function App() {
 
         <Route path="/products" element={<Products />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/product-details/:id" element={<ProductDetails />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
---
+++ router-dom/src/pages/Products.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Product from '../components/Product'
+import { products } from '../data/products'
+
+const Products = () => {
+    return (
+        <div>
+            {products && products.map((product) => (
+                <Product key={product.id} product={product} />
+            ))}
+        </div>
+    )
+}
+
+export default Products
