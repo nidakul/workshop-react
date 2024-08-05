0c033818a84da48c3681d0097b26bbb398cb2c99
nidakul <nidakul@hotmail.com>
2024-08-05 18:01:30 +0300

    created useToggle

--- custom-hook/src/App.jsx
+++ custom-hook/src/App.jsx
@@ -3,17 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import useCounter from './hooks/useCounter'
+import useToggle from './hooks/useToggle'
 
 function App() {
   const { count, increase, decrease } = useCounter();
+  const { open, change } = useToggle();
+
   return (
     <>
       {/* <div>{count}</div>
       <button onClick={increase}>Arttır</button>
       <button onClick={decrease}>Azalt</button> */}
-      <div style={{ width }}>
-
-      </div>
+      {/* **************** */}
+      {/* {open && <div style={{ width: '100px', height: '100px', backgroundColor: 'red' }} />}
+      <button onClick={change}>{open ? 'Gizle' : 'Göster'}</button> */}
     </>
   )
 }
