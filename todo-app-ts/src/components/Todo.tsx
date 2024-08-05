98fc761aa02a8b96410ad63f376829b6c57c3af4
nidakul <nidakul@hotmail.com>
2024-08-05 15:20:18 +0300

    removed todo

--- todo-app-ts/src/components/Todo.tsx
+++ todo-app-ts/src/components/Todo.tsx
@@ -3,18 +3,26 @@ import { IoMdRemoveCircleOutline } from "react-icons/io";
 import { FaCheck } from "react-icons/fa";
 import { FaRegEdit } from "react-icons/fa";
 import { TodoType } from '../types/Types';
+import { useDispatch } from 'react-redux';
+import { removeTodoById } from '../redux/todoSlice';
 
 interface TodoProps {
     todoProps: TodoType
 }
 
 const Todo = ({ todoProps }: TodoProps) => {
+    const dispatch = useDispatch();
     const { id, content } = todoProps;
+
+    const handleRemoveTodo = () => {
+        dispatch(removeTodoById(id));
+    }
+
     return (
         <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', border: '1px solid lightgrey', padding: '16px', marginTop: '25px' }}>
             <div>{content}</div>
             <div>
-                <IoMdRemoveCircleOutline />
+                <IoMdRemoveCircleOutline onClick={handleRemoveTodo} />
                 <FaRegEdit />
             </div>
         </div>
