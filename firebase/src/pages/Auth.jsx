a64d4fa00a37ebadf4aa0620635e9630a08df466
nidakul <nidakul@hotmail.com>
2024-08-21 00:27:14 +0300

    login firebase

--- firebase/src/pages/Auth.jsx
+++ firebase/src/pages/Auth.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import { FaGoogle } from "react-icons/fa";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { toast } from 'react-toastify';
 import { auth } from '../Firebase';
+import { useNavigate } from 'react-router-dom';
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     const register = async () => {
         try {
@@ -21,6 +23,18 @@ const Auth = () => {
             toast.error(error.message)
         }
     }
+
+    const login = async () => {
+        try {
+            const response = await signInWithEmailAndPassword(auth, email, password)
+            const user = response.user
+            if (user) {
+                navigate("/")
+            }
+        } catch (error) {
+            toast.error("Giriş yapılamadı " + error.message)
+        }
+    }
     return (
         <div className='auth'>
             <h3 className='auth-header'>Giriş Yap / Kaydol</h3>
@@ -30,7 +44,7 @@ const Auth = () => {
             </div>
             <div className='button-div'>
                 <button className='google-button'><FaGoogle style={{ marginTop: '2px', marginRight: '5px' }} />Google İle Giriş</button>
-                <button className='login-button'>Giriş Yap</button>
+                <button onClick={login} className='login-button'>Giriş Yap</button>
                 <button onClick={register} className='register-button'>Kaydol</button>
             </div>
         </div>
