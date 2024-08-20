810c072a35b5742e95ecc9765f62352b9dd3ed80
nidakul <nidakul@hotmail.com>
2024-08-21 00:43:08 +0300

    logout firebase

--- firebase/src/components/Navbar.jsx
+++ firebase/src/components/Navbar.jsx
@@ -1,12 +1,24 @@
+import { signOut } from 'firebase/auth'
 import React from 'react'
+import { auth } from '../Firebase';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const Navbar = () => {
+    const navigate = useNavigate();
+    const logout = async () => {
+        toast.success("Çıkış yapılıyor")
+        setTimeout(() => {
+            signOut(auth)
+            navigate("/auth")
+        }, 3000);
+    }
     return (
         <div className='navbar'>
             <div className='navbar-left'>
                 FIREBASE
             </div>
-            <div className='navbar-right'>Çıkış Yap</div>
+            <div onClick={logout} className='navbar-right'>Çıkış Yap</div>
         </div>
     )
 }
