e3349230f196322ceae0c53ce3f4463c29ba39af
nidakul <nidakul@hotmail.com>
2024-08-29 21:04:35 +0300

    created validation schema

---
+++ formik-yup/src/schemas/RegisterFormSchemas.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as yup from 'yup';
+
+export const registerFormSchemas = yup.object().schema({
+    email: yup.string().email("Geçerli email adresi giriniz.").required("Email adresi zorunlu"),
+    age: yup.number().positive("Pozitif bir değer giriniz").integer("Tam sayı giriniz").required("Yaş alanı zorunlu"),
+    password: yup.string().required("Şifre alanı zorunlu"),
+    confirmPassword: yup.string().required("Şifre tekrarı zorunlu")
+        .oneOf([yup.ref('password', yup.password)]),
+    term: yup.boolean().required("Lütfen kutucuğu onaylayınız")
+}) 
